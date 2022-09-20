419a796246a7ef11dbfbccbcf7515d0f6a04fad5
mrleonardobrito <lbritogit@gmail.com>
2022-09-20 14:17:22 -0300

    testando a rapidez

--- sudoku/cmd/commandline/main.go
+++ sudoku/cmd/commandline/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
 	gamesrv "sodouku/internal/core/service"
 	"sodouku/internal/handlers/gamehdl"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	gameService := gamesrv.New()
 	gameHandler := gamehdl.NewCMDHandler(gameService)
 
 	gameHandler.NewGame("jogo teste", 10, "easy")
+	timeTrack(start, "main")
+}
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start).String()
+	log.Printf("%s took %s", name, elapsed)
 }
